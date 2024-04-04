63ce196886c1cd94de6d01647ccd7af41d979fd6
Enzo Russo <enzo.russo1@etudiant.univ-reims.fr>
2024-04-04 09:41:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Web Documentaire - Start</title>
+  <link rel="stylesheet" type="text/css" href="../styles.css">
+</head>
+<body>
+<img class="background_1" src="../background.jpg">
+<div style="display: flex; align-items: center; justify-content: center;">
+  <h1 style="    color: #fff;
+      font-size: 130px;
+      width: 80%;
+      padding-top: 200px;
+      padding-left: 75px;">LE SHERIFF JOHN</h1>
+
+  <a href="#" >
+    <img src="../start.png" style="    width: 200px;">
+  </a>
+</div>
+</body>
+</html>
